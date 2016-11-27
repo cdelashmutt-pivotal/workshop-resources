c742058165d6f55acee96f1ec61c1f9b5f2a1e1b
Chris DeLashmutt <cdelashmutt@pivotal.io>
2016-11-27 18:16:29 -0500

    Adding simple ASP App for push, scale, monitor labs

---
+++ ASPSimple/ASPSimple/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using ASPSimple.ViewModels;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPSimple.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var cloudFoundryApplication = ServerConfig.CloudFoundryApplication;
+            var cloudFoundryServices = ServerConfig.CloudFoundryServices;
+            return View(new CloudFoundryViewModel(
+                cloudFoundryApplication == null ? new CloudFoundryApplicationOptions() : cloudFoundryApplication,
+                cloudFoundryServices == null ? new CloudFoundryServicesOptions() : cloudFoundryServices));
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
